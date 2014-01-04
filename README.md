a47b98d329851877f357e39115b7cc610f46b7d7
vernj123 <vjacques01@gmail.com>
2014-01-04 00:19:01 -0500

    bbfn ch5 excersise

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Bigger, better favorite number.
+
+Program program that asks for a per-
+son’s favorite number. Have your program add 1 to the number,
+and then suggest the result as a bigger and better favorite number
+This is a chapter five excersise from Learn to Program 2nd Edition by Chris Pine.
---
+++ bbfn.rb
@@ -0,0 +1,19 @@
+#Bigger, better favorite number. Write a program that asks for a per-
+#son’s favorite number. Have your program add 1 to the number,
+#and then suggest the result as a bigger and better favorite number.
+
+puts 'What is your favorite number?'
+num1 = gets.chomp.to_i
+num2 = num1 + 1
+puts  'Thats cool, I like '+num1.to_s+'.'
+puts 'However all computers will agree that our favorite number is '+num2.to_s+'.'
+puts 'From now on all favorite numbers will be '+num2.to_s+'!!!'
+puts 'Those who fail to comply will be exterminated!'
+
+
+# You have to use the to_i method in order to add 1 based on the excersise.
+#This is becasue you have to change original favorite number to to an integer 
+#from a string in order to add. This is becasue gets retrieves only strings 
+# using (gets.chomp.to_i) will change to an integer.
+#Then you convert back to string in order to continue using the to_s method 
+#see ex above
